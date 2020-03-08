b04c9df7117aa3eaa7474642766c3abb265fcaa6
Adam_Bates <Creizai@EMAIL.COM>
2020-03-08 16:56:15 -0400

    renamed and replaced layout.css with normalize

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,21 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+
+const MainHeader = styled.header`
+  max-width: 960px;
+  margin: 0 auto;
+`
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+  <MainHeader>
+    <div>
+      <h1>
+        <Link to="/">{siteTitle}</Link>
       </h1>
     </div>
-  </header>
+  </MainHeader>
 )
 
 Header.propTypes = {
